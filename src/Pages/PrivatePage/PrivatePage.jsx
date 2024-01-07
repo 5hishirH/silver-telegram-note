6b8f124f0ed08844a57d4bd82a6b75a41884295d
Shishir Halder <s.halder0108@gmail.com>
2024-01-07 19:17:18 +0600

    feat: create private route

---
+++ src/Pages/PrivatePage/PrivatePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuthContext from "../../CustomHooks/useAuthContext";
+
+const PrivatePage = ({ children }) => {
+  const { loading, user } = useAuthContext();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+      </div>
+    );
+  }
+  else if (user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
+};
+
+export default PrivatePage;
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import PrivatePage from "../Pages/PrivatePage/PrivatePage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
+    element: (
+      <PrivatePage>
+        <MainLayout />
+      </PrivatePage>
+    ),
     children: [
       {
         path: "/",
-        element: <Home />
-      }
-    ]
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
